f3fdb22b646d7213d47b5e6011ac846ed0450d92
FCS <37077211+fcxsantos@users.noreply.github.com>
2018-03-19 18:14:33 -0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,17 +1,7 @@
-from django.conf.urls import include, url
-from django.conf.urls.static import static
-from django.conf import settings
-
-from callapp import views
-
+from django.conf.urls import url, include
+from django.contrib import admin
+ 
 urlpatterns = [
-url(r'^$', views.CallList.as_view(), name='call_list'),
-url(r'^bill', views.CallBill.as_view(), name='call_bill'),
-url(r'^bill/(?P<phone>\d+)$', views.CallBill.as_view(), name='call_bill'),
-url(r'^new', views.CallCreate.as_view(), name='call_new'),
-url(r'^edit/(?P<pk>\d+)$', views.CallUpdate.as_view(), name='call_edit'),
-url(r'^delete/(?P<pk>\d+)$', views.CallDelete.as_view(), name='call_delete'),
-url(r'^end/(?P<pk>\d+)$', views.CallEnd.as_view(), name='call_end'),
+ 
+    url(r'^callapp/', include('callapp.urls')),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
